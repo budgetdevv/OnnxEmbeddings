4e5484c7d6bf8c75be6ba36244e3218ebb9aec96
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-07-12 00:37:08 +0800

    Fixed TorchHelpers.NormalizeTensor()

--- OnnxEmbeddings/Helpers/TorchHelpers.cs
+++ OnnxEmbeddings/Helpers/TorchHelpers.cs
@@ -1,13 +1,15 @@
-﻿using System;
-
-namespace OnnxEmbeddings.Helpers
+﻿namespace OnnxEmbeddings.Helpers
 {
     public static class TorchHelpers
     {
         // This is from PyTorch's `torch.nn.functional.normalize` function.
-        public static TorchTensor NormalizeTensor(TorchTensor input, float p = 2.0f, int dim = 1, float eps = 1e-12f, TorchTensor? output = null)
+        public static TorchTensor NormalizeTensor(TorchTensor input, float p = 2.0f, int dim = 1, float eps = 1e-12f)
         {
-            var denom = input.norm(p: p, dim: dim, keepdim: true).clamp_min(eps).expand_as(input);
+            var denom = input
+                .norm(p: p, dim: dim, keepdim: true)
+                .clamp_min(eps)
+                .expand_as(input);
+            
             return input / denom;
         }
     }

6c0bce8ee23f68a602bceeffc33f2c241438ef14
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-07-12 17:30:12 +0800

    - Use ROS for dimensions in PoolingHelpers.MeanPooling()
    - MiniLML6V2.cs now uses SentenceEmbedder

--- OnnxEmbeddings/Helpers/PoolingHelpers.cs
+++ OnnxEmbeddings/Helpers/PoolingHelpers.cs
@@ -1,25 +1,27 @@
-﻿namespace OnnxEmbeddings.Helpers
+﻿using System;
+
+namespace OnnxEmbeddings.Helpers
 {
     public static class PoolingHelpers
     {
         public static TorchTensor MeanPooling(
             float[] tokenEmbeddings,
             long[] attentionMask,
-            long[] attentionMaskDimensions,
-            long[] tokenEmbeddingsDimensions)
+            ReadOnlySpan<long> attentionMaskDimensions,
+            ReadOnlySpan<long> tokenEmbeddingsDimensions)
         {
             var tokenEmbeddingsTensor = Torch.tensor(rawArray: tokenEmbeddings, dimensions: tokenEmbeddingsDimensions);
 
             var attentionMaskExpandedTensor = Torch
                 .tensor(dataArray: attentionMask, dimensions: attentionMaskDimensions)
-                .unsqueeze(-1)
-                .expand(tokenEmbeddingsTensor.shape)
+                .unsqueeze(dim: -1)
+                .expand(sizes: tokenEmbeddingsTensor.shape)
                 .to(Torch.float32);
 
             var sumEmbeddings = (tokenEmbeddingsTensor * attentionMaskExpandedTensor).sum(1);
 
             var sumMask = attentionMaskExpandedTensor
-                .sum(1)
+                .sum(dim: 1)
                 .clamp(min: 1e-9, max: float.MaxValue);
 
             return sumEmbeddings / sumMask;
--- OnnxEmbeddings/Models/HuggingFace/MiniLML6V2.cs
+++ OnnxEmbeddings/Models/HuggingFace/MiniLML6V2.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FastBertTokenizer;
-using Microsoft.ML;
-using Microsoft.ML.Data;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 using OnnxEmbeddings.Helpers;
 using OnnxEmbeddings.Tokenizer;
 
@@ -13,48 +13,39 @@ namespace OnnxEmbeddings.Models.HuggingFace
     public sealed class MiniLML6V2<ConfigT>: IHuggingFaceModel<MiniLML6V2<ConfigT>, ConfigT> 
         where ConfigT: struct, IModelConfig
     {
-        private class Input(long[] inputIDs, long[] attentionMask)
+        private struct Output: SentenceEmbedder.ISentenceEmbedderOutput
         {
-            // Dimensions: batch, sequence
-            // ReSharper disable once UnusedMember.Local ( It is used by ML.NET )
-            [ColumnName(INPUT_IDS)]
-            public long[] InputIDs { get; } = inputIDs;
-
-            // Dimensions: batch, sequence
-            [ColumnName(ATTENTION_MASK)]
-            public long[] AttentionMask { get; } = attentionMask;
-        }
-    
-        private sealed class Output
-        {
+            public const string 
+                TOKEN_EMBEDDINGS = "token_embeddings",
+                SENTENCE_EMBEDDING = "sentence_embedding";
+            
+            public static string[] OutputNames => [ TOKEN_EMBEDDINGS, SENTENCE_EMBEDDING ];
+            
             // Dimensions: batch_size, sequence, 384
-            // ReSharper disable once UnusedAutoPropertyAccessor.Local ( It is used by ML.NET )
-            [ColumnName(TOKEN_EMBEDDINGS)]
             public float[] TokenEmbeddings { get; set; }
             
             // Dimensions: batch_size, Divsentence_embedding_dim_1
-            // ReSharper disable once UnusedAutoPropertyAccessor.Local ( It is used by ML.NET )
-            [ColumnName(SENTENCE_EMBEDDING)]
             public float[] SentenceEmbeddings { get; set; }
+            
+            public void PopulateOutput(IDisposableReadOnlyCollection<DisposableNamedOnnxValue> output)
+            {
+                foreach (var value in output)
+                {
+                    var arr = ((DenseTensor<float>) value.Value).ToArray();
+                    
+                    switch (value.Name)
+                    {
+                        case TOKEN_EMBEDDINGS:
+                            TokenEmbeddings = arr;
+                            break;
+                        
+                        case SENTENCE_EMBEDDING:
+                            SentenceEmbeddings = arr;
+                            break;
+                    }
+                }
+            }
         }
-        
-        private const string 
-            INPUT_IDS = "input_ids",
-            ATTENTION_MASK = "attention_mask",
-            TOKEN_EMBEDDINGS = "token_embeddings",
-            SENTENCE_EMBEDDING = "sentence_embedding";
-
-        private static readonly string[] INPUT_COLUMN_NAMES =
-        [
-            INPUT_IDS,
-            ATTENTION_MASK,
-        ];
-        
-        private static readonly string[] OUTPUT_COLUMN_NAMES = 
-        [ 
-            TOKEN_EMBEDDINGS,
-            SENTENCE_EMBEDDING,
-        ];
         
         public static int MAX_SEQUENCE_LENGTH => 256;
         public static int EMBEDDING_DIMENSION => 384;
@@ -62,10 +53,13 @@ namespace OnnxEmbeddings.Models.HuggingFace
         public static string HuggingFaceRepoName => "sentence-transformers/all-MiniLM-L6-v2";
         
         private readonly BertTokenizer WordPieceTokenizer;
+        
+        private readonly SentenceEmbedder<SentenceEmbedder.Input, Output> Embedder;
 
         private MiniLML6V2(BertTokenizer wordPieceTokenizer)
         {
             WordPieceTokenizer = wordPieceTokenizer;
+            Embedder = new(ConfigT.ModelPath);
         }
         
         public static async ValueTask<MiniLML6V2<ConfigT>> LoadModelAsync()
@@ -90,12 +84,6 @@ namespace OnnxEmbeddings.Models.HuggingFace
             bool normalize,
             out int[] sentenceEmbeddingDimensions)
         {
-            var mlContext = new MLContext();
-            
-            var batchSize = sentences.Length;
-            
-            var encodedCorpus = CreateInput(sentences, maxSequenceLength);
-            
             if (maxSequenceLength > MAX_SEQUENCE_LENGTH)
             {
                 throw new ArgumentException(
@@ -103,71 +91,28 @@ namespace OnnxEmbeddings.Models.HuggingFace
                     nameof(maxSequenceLength));
             }
             
-            var encodedCorpusAttentionMask = encodedCorpus.AttentionMask;
+            var batchSize = sentences.Length;
+            
+            var input = new SentenceEmbedder.Input(sentences, maxSequenceLength, WordPieceTokenizer);
+            
+            var attentionMask = input.AttentionMask;
+            
+            var attentionMaskDimensionsLong = (ReadOnlySpan<long>) input.Dimensions
+                .ExpandToLong()
+                .AsSpan();
 
-            // Onnx models do not support variable dimension vectors.
-            var inputSchema = SchemaDefinition.Create(typeof(Input));
+            var output = Embedder.GenerateEmbeddings(input);
 
-            int[] inputIDsDimensions = [ batchSize, maxSequenceLength ];
-            
-            inputSchema[INPUT_IDS].ColumnType = new VectorDataViewType(
-                itemType: NumberDataViewType.Int64, 
-                dimensions: inputIDsDimensions);
-            
-            int[] attentionMaskDimensions = [ batchSize, maxSequenceLength ];
-            
-            inputSchema[ATTENTION_MASK].ColumnType = new VectorDataViewType(
-                itemType: NumberDataViewType.Int64,
-                dimensions: attentionMaskDimensions);
-            
-            // Onnx models may have hardcoded dimensions for inputs. Use a custom
-            // schema for variable dimension since the number of text documents
-            // are a user input for us (batchSize).
-            var inputShape = new Dictionary<string, int[]>
-            {
-                { INPUT_IDS, inputIDsDimensions },
-                { ATTENTION_MASK, attentionMaskDimensions },
-            };
-            
-            var pipeline = mlContext.Transforms
-                .ApplyOnnxModel(
-                    inputColumnNames: INPUT_COLUMN_NAMES,
-                    outputColumnNames: OUTPUT_COLUMN_NAMES,
-                    modelFile: ConfigT.ModelPath,
-                    shapeDictionary: inputShape,
-                    gpuDeviceId: null,
-                    fallbackToCpu: true);
-            
-            var trainingData = mlContext.Data.LoadFromEnumerable<Input>([ ], inputSchema);
-            var model = pipeline.Fit(trainingData);
-            
-            var outputSchema = SchemaDefinition.Create(typeof(Output));
+            var tokenEmbeddings = output.TokenEmbeddings;
+            var sentenceEmbeddings = output.SentenceEmbeddings;
 
             int[] tokenEmbeddingsDimensions = [ batchSize, maxSequenceLength, EMBEDDING_DIMENSION ];
             
-            outputSchema[TOKEN_EMBEDDINGS].ColumnType = new VectorDataViewType(
-                itemType: NumberDataViewType.Single, 
-                dimensions: tokenEmbeddingsDimensions);
+            var tokenEmbeddingsDimensionsLong = (ReadOnlySpan<long>) tokenEmbeddingsDimensions
+                .ExpandToLong()
+                .AsSpan();
             
             sentenceEmbeddingDimensions = [ batchSize, EMBEDDING_DIMENSION ];
-            
-            outputSchema[SENTENCE_EMBEDDING].ColumnType = new VectorDataViewType(
-                itemType: NumberDataViewType.Single, 
-                dimensions: sentenceEmbeddingDimensions);
-            
-            var engine = mlContext.Model
-                .CreatePredictionEngine<Input, Output>(
-                    transformer: model,
-                    inputSchemaDefinition: inputSchema,
-                    outputSchemaDefinition: outputSchema);
-            
-            var prediction = engine.Predict(encodedCorpus);
-
-            var tokenEmbeddings = prediction.TokenEmbeddings;
-            var sentenceEmbeddings = prediction.SentenceEmbeddings;
-
-            var tokenEmbeddingsDimensionsLong = tokenEmbeddingsDimensions.ExpandToLong();
-            var attentionMaskDimensionsLong = attentionMaskDimensions.ExpandToLong();
 
             // See: https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
             
@@ -175,7 +120,7 @@ namespace OnnxEmbeddings.Models.HuggingFace
             {
                 var tensor = PoolingHelpers.MeanPooling(
                     tokenEmbeddings,
-                    encodedCorpusAttentionMask,
+                    attentionMask,
                     attentionMaskDimensionsLong,
                     tokenEmbeddingsDimensionsLong);
                 
@@ -201,33 +146,10 @@ namespace OnnxEmbeddings.Models.HuggingFace
                 }
             }
         }
-
-        private Input CreateInput(string sentence, int maxSequenceLength)
-        {
-            var (inputIDs, attentionMask, _) = WordPieceTokenizer
-                .Encode(input: sentence, maximumTokens: maxSequenceLength);
-            
-            return new(inputIDs.ToArray(), attentionMask.ToArray());
-        }
-
-        private Input CreateInput(string[] sentences, int maxSequenceLength)
-        {
-            var batchSize = sentences.Length;
-            var bufferSize = batchSize * maxSequenceLength;
-            
-            // Allocate memory for inputIds and attentionMask
-            var inputIDs = new long[bufferSize];
-            var attentionMask = new long[bufferSize];
-            
-            // Encode the sentences using the provided method
-            WordPieceTokenizer.Encode(sentences, inputIDs, attentionMask, maxSequenceLength);
-            
-            return new(inputIDs: inputIDs, attentionMask: attentionMask);
-        }
         
         public void Dispose()
         {
-            // TODO release managed resources here
+            Embedder.Dispose();
         }
     }
 }
